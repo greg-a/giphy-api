76822a096c7d864912e99376f043ea5d28f87f76
Greg Allebach <greg.allebach@gmail.com>
2020-05-14 17:15:00 -0400

    added some html elements to set up favorite and delete gifs and topics

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,13 @@
-var topics = ["Bojack Horseman", "Breaking Bad", "Burn After Reading", "The Office"];
+var topics = ["Arrested Development", "Breaking Bad", "Burn After Reading", "Bojack Horseman"];
+var used = [];
 
 function addTopics() {
     for (var i = 0; i < topics.length; i++) {
         var lineItem = $("<li>");
+        var delTopic = $("<span>").text("X").addClass("list-group-item").attr("id", "x-button");
 
-        lineItem.text(topics[i]).addClass("list-group-item");
+        lineItem.text(topics[i]).addClass("list-group-item line-item").attr("data-name", topics[i]);
+        lineItem.append(delTopic);
         $(".list-group").prepend(lineItem);
     }
 }
@@ -16,9 +19,8 @@ function clearList() {
 $(document).on("click", ".list-group-item", function(event) {
     event.preventDefault();
     
-    var searchName = this.innerHTML;
+    var searchName = this.dataset.name;
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchName + "&limit=10&api_key=5v4rQS10XtSYgP0Dv4jnobu5HtcEQDWA";
-
 
     $.ajax({
         url: queryURL,
@@ -26,19 +28,33 @@ $(document).on("click", ".list-group-item", function(event) {
     })
     .then(function(response) {
         var results = response.data;
-        
-        for (var i = 0; i < results.length; i++) {
-            var gifDiv = $("<div>");
-            var rating = results[i].rating;
-            var p = $("<p>").text("Rating: " + rating.toUpperCase());
+        console.log(queryURL);
 
-            var personImage = $("<img>");
-            personImage.attr("src", results[i].images.fixed_height_still.url).attr("data-status", "still").attr("data-still", results[i].images.fixed_height_still.url).attr("data-active", results[i].images.fixed_height.url).addClass("this-gif");
+        for (var w = 0; w < 10; w++) {
+            if (results[w].id != used[w]) {
 
-            gifDiv.prepend(p);
-            gifDiv.prepend(personImage);
-
-            $(".gif-container").prepend(gifDiv);
+                // for (var i = 0; i < results.length; i++) {
+                    var gifDiv = $("<div>").addClass("gif-div");
+                    var rating = results[w].rating;
+                    var p = $("<p>").text("Rating: " + rating.toUpperCase()).addClass("rating under");
+                    var fav = $("<p>").text("Add Fav").addClass("fav-button under");
+                    var del = $("<p>").text("Delete").addClass("del-button under");
+                    var underGif = $("<div>").addClass("under-gif");
+                    var personImage = $("<img>");
+                    
+                    personImage.attr("src", results[w].images.fixed_height_still.url).attr("data-status", "still").attr("data-still", results[w].images.fixed_height_still.url).attr("data-active", results[w].images.fixed_height.url).addClass("this-gif");
+                    
+                    
+                    underGif.prepend(p, fav, del);
+                    gifDiv.prepend(personImage);
+                    gifDiv.append(underGif);
+                    
+                    $(".gif-container").prepend(gifDiv);
+                    
+                    used.push(results[w].id);
+                    
+                // }
+            }
         }
     })
 })
