bb5435cbc1aec9d97f5f7ce90c7b0d84a5650401
Greg Allebach <greg.allebach@gmail.com>
2020-05-16 22:37:52 -0400

    set up functionality for adding favs, deleting gifs, deleting topics, and utilizing local storage to remember topics the user added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,14 @@
-var topics = ["Arrested Development", "Breaking Bad", "Burn After Reading", "Bojack Horseman"];
+var defaultTopics = ["Arrested Development", "Breaking Bad", "Burn After Reading", "Bojack Horseman"];
 var used = [];
+var userTopics = [];
 
 function addTopics() {
     for (var i = 0; i < topics.length; i++) {
-        var lineItem = $("<li>");
-        var delTopic = $("<span>").text("X").addClass("list-group-item").attr("id", "x-button");
+        var lineItemContainer = $("<span>").addClass("li-span").attr("data-name", topics[i]);
+        var lineItem = $("<li>").text(topics[i]).addClass("list-group-item line-item").attr("data-name", topics[i]);
+        var delTopic = $("<span>").text("X").addClass("list-group-item").attr("id", "x-button").attr("data-name", topics[i]);
 
-        lineItem.text(topics[i]).addClass("list-group-item line-item").attr("data-name", topics[i]);
-        lineItem.append(delTopic);
+        lineItem.append(delTopic, lineItemContainer);
         $(".list-group").prepend(lineItem);
     }
 }
@@ -16,52 +17,72 @@ function clearList() {
     $(".list-group").empty();
 }
 
-$(document).on("click", ".list-group-item", function(event) {
+$(document).on("click", ".li-span", function (event) {
     event.preventDefault();
-    
+
     var searchName = this.dataset.name;
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchName + "&limit=10&api_key=5v4rQS10XtSYgP0Dv4jnobu5HtcEQDWA";
-
+    console.log(queryURL);
     $.ajax({
         url: queryURL,
         method: "GET"
     })
-    .then(function(response) {
-        var results = response.data;
-        console.log(queryURL);
+        .then(function (response) {
+            var results = response.data;
 
-        for (var w = 0; w < 10; w++) {
-            if (results[w].id != used[w]) {
+            for (var i = 0; i < results.length; i++) {
+                var gifDiv = $("<div>").addClass("gif-div").attr("id", results[i].id);
+                var p = $("<p>").text("Rating: " + results[i].rating.toUpperCase()).addClass("rating under");
+                var fav = $("<p>").text("Add Fav").addClass("fav-button under").attr("data-name", results[i].id);
+                var del = $("<p>").text("Delete").addClass("del-button under").attr("data-delete", results[i].id);
+                var underGif = $("<div>").addClass("under-gif");
+                var personImage = $("<img>").attr("src", results[i].images.fixed_height_still.url).attr("data-status", "still").attr("data-still", results[i].images.fixed_height_still.url).attr("data-active", results[i].images.fixed_height.url).addClass("this-gif");
 
-                // for (var i = 0; i < results.length; i++) {
-                    var gifDiv = $("<div>").addClass("gif-div");
-                    var rating = results[w].rating;
-                    var p = $("<p>").text("Rating: " + rating.toUpperCase()).addClass("rating under");
-                    var fav = $("<p>").text("Add Fav").addClass("fav-button under");
-                    var del = $("<p>").text("Delete").addClass("del-button under");
-                    var underGif = $("<div>").addClass("under-gif");
-                    var personImage = $("<img>");
-                    
-                    personImage.attr("src", results[w].images.fixed_height_still.url).attr("data-status", "still").attr("data-still", results[w].images.fixed_height_still.url).attr("data-active", results[w].images.fixed_height.url).addClass("this-gif");
-                    
-                    
-                    underGif.prepend(p, fav, del);
-                    gifDiv.prepend(personImage);
-                    gifDiv.append(underGif);
-                    
-                    $(".gif-container").prepend(gifDiv);
-                    
-                    used.push(results[w].id);
-                    
-                // }
+                underGif.prepend(p, fav, del);
+                gifDiv.prepend(personImage);
+                gifDiv.append(underGif);
+
+                $(".gif-container").prepend(gifDiv);
+
+                used.push(results[i]);
             }
-        }
-    })
+            $(".gif-container").prepend("<h2>New GIFs</h2>");
+        })
 })
 
-$(document).on("click", ".this-gif", function(event){
+$(document).on("click", ".fav-button", function (event) {
+
+    for (var i = 0; i < used.length; i++) {
+        if ($(this).attr("data-name") === used[i].id) {
+            var favGifDiv = $("<div>").addClass("gif-div").attr("id", used[i].id);
+            var favGif = $("<img>").attr("src", used[i].images.fixed_height_still.url).attr("data-status", "still").attr("data-still", used[i].images.fixed_height_still.url).attr("data-active", used[i].images.fixed_height.url).addClass("this-gif");
+            var p = $("<p>").text("Rating: " + used[i].rating.toUpperCase()).addClass("rating under");
+            var del = $("<p>").text("Delete").addClass("del-button under").attr("data-delete", used[i].id);
+            var underGif = $("<div>").addClass("under-gif");
+
+            underGif.prepend(p, del);
+            favGifDiv.append(favGif);
+            favGifDiv.append(underGif);
+            $(".fav-gifs").prepend(favGifDiv);
+            $("#" + used[i].id).remove();
+        }
+    }
+    $(".fav-gifs h2").remove();
+    $(".fav-gifs").prepend("<h2>Fav GIFs</h2>");
+})
+
+$(document).on("click", ".del-button", function (event) {
+    for (var i = 0; i < used.length; i++) {
+        if ($(this).attr("data-delete") === used[i].id) {
+            $("#" + used[i].id).remove();
+        }
+    }
+    console.log($(this).attr("data-delete"))
+})
+
+$(document).on("click", ".this-gif", function (event) {
     var status = $(this).attr("data-status");
-    
+
     if (status === "still") {
         $(this).attr("src", $(this).attr("data-active")).attr("data-status", "active")
     }
@@ -70,18 +91,57 @@ $(document).on("click", ".this-gif", function(event){
     }
 })
 
-$("#button-addon2").on("click", function(event) {
-    var newTopic = $(".form-control").val().trim();
-    $(".form-control").val("");
-    topics.push(newTopic);
+$(document).on("click", "#x-button", function (event) {
+    topics = topics.filter(e => e !== $(this).attr("data-name"));
+    userTopics = topics.filter(e => e !== $(this).attr("data-name"));
     clearList();
-    addTopics();   
+    addTopics();
+    localStorage.setItem("user topics", JSON.stringify(userTopics));
+    $(".gif-container").empty();
 })
 
-$("#button-addon3").on("click", function(event) {
+$("#button-addon2").on("click", function (event) {
+    var newTopic = $(".form-control").val().trim();
+
+    $(".form-control").val("");
+
+    topics.push(newTopic);
+    clearList();
+    addTopics();
+    userTopics.push(newTopic);
+
+    localStorage.setItem("user topics", JSON.stringify(userTopics));
+})
+
+$("#form-id").on("keyup", function (event) {
+    if (event.keyCode === 13) {
+        var newTopic = $(".form-control").val().trim();
+
+        $(".form-control").val("");
+
+        topics.push(newTopic);
+        clearList();
+        addTopics();
+        userTopics.push(newTopic);
+
+        localStorage.setItem("user topics", JSON.stringify(userTopics));
+    }
+})
+
+$("#button-addon3").on("click", function (event) {
     topics = [];
     clearList();
-    console.log(topics);
+    $(".gif-container").empty();
+    $(".fav-gifs").empty();
 })
+
+var topics = JSON.parse(localStorage.getItem("user topics"));
+
+if (!Array.isArray(topics) || topics.length === 0) {
+    topics = defaultTopics;
+}
+else {
+    userTopics = JSON.parse(localStorage.getItem("user topics"))
+}
 
 addTopics();
